731a93df13080e04bfb493ac15eea95f635f1563
Gurba,Chris <Chris.Gurba@Cerner.com>
2016-08-09 10:21:12 -0500

    add notes on 09

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -8,6 +8,21 @@ const mapStateToProps = (state, { params }) => ({
   todos: getVisibleTodos(state, params.filter || 'all'),
 });
 
+// deleted :
+
+// const mapDispatchToProps = (dispatch) => ({
+//   onTodoClick(id): {
+//     dispatch(toggleTodo(id));
+//   },
+// });
+
+// common case for mapDispatchToProps, sometimes mapState:
+// arguments to the callback of mapDispatch match the
+// arguments of the action creator (aka 'id' above),
+// we can use the shorthand below to simply say
+// the callback prop(s) (key(s)) should dispatch the action
+// created by the action creator(s) (value(s)), in the object
+// passed.
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
