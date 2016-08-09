1dae0014e3c7dabf24d25c9e159f19f344fcaa86
Gurba,Chris <Chris.Gurba@Cerner.com>
2016-08-09 12:31:58 -0500

    notes on 16, 17

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 import { v4 } from 'node-uuid';
 import * as api from '../api';
-
+// now we can call api functions inside actions.
 const receiveTodos = (filter, response) => ({
   type: 'RECEIVE_TODOS',
   filter,
   response,
 });
-
+// "asynchronous" action creator.  calls fetch, waits on promise resolve
+// then hands back receiveTodos call on filter, response
 export const fetchTodos = (filter) =>
   api.fetchTodos(filter).then(response =>
     receiveTodos(filter, response)
--- src/configureStore.js
+++ src/configureStore.js
@@ -18,14 +18,40 @@ const logger = (store) => (next) => {
   };
   /* eslint-enable no-console */
 };
+// next is the dispatch at that current moment.
+// can't call it rawDispatch anymore because it might (and has) been modified by
+// other middlewares.
 
+// function that returns a function that returns afunction
+// alternate syntax : 
+// const addPromiseSupportToDispatch = (store) => {
+//   return (next) => {
+//     return (action) {
+//       if (typeof action.then === 'function') {
+//         return action.then(next);
+//       }
+//       return next(action);
+//     };
+//   };
+// };
+// mental model to digest this syntax below : consider it
+// a function with multiple arguments that are used as they come
+// available.  
 const promise = (store) => (next) => (action) => { // eslint-disable-line no-unused-vars
+  // wrap to see if action is of type promise... we don't want a promise object so we call
+  // promise's then()
   if (typeof action.then === 'function') {
     return action.then(next);
   }
   return next(action);
 };
 
+// for every middleware, override the store.dispatch function with
+// the curried middleware function.  
+// consider this function being called with promise above:
+// store is the store, next is the dispatch, and this assigns
+// store.dispatch to the action function declared above as the most
+// deeply nested function.
 const wrapDispatchWithMiddlewares = (store, middlewares) =>
   middlewares.slice().reverse().forEach(middleware => {
     store.dispatch = middleware(store)(store.dispatch); // eslint-disable-line no-param-reassign
@@ -33,9 +59,14 @@ const wrapDispatchWithMiddlewares = (store, middlewares) =>
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  // specify middlewares in the order in which the action travels through them.
+  // we then need to call reverse in our wrapdispatch to make sure the 
+  // wrapping logic order is preserved.  Logger wraps base store.dispatch,
+  // then promise wraps logger dispatch.  Not sure why order matters here.
   const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
+    // array of functions to be applied as middleware
     middlewares.push(logger);
   }
 
