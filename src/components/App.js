18c6473dc65ac3b2cbb4f55b2db28036132c74a8
Gurba,Chris <Chris.Gurba@Cerner.com>
2016-08-09 10:08:57 -0500

    notes on 08

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import Footer from './Footer';
 import AddTodo from './AddTodo';
 import VisibleTodoList from './VisibleTodoList';
 
+// we were able to remove the { params } from the app component,
+// and not pass it as a param to VisibleTodoList because withRouter
+// can pull it from App's parent route
 const App = () => (
   <div>
     <AddTodo />
--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+// requires react-router 3.0.0 (alpha right now)
 import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
@@ -15,11 +16,17 @@ const getVisibleTodos = (todos, filter) => {
       throw new Error(`Unknown filter: ${filter}.`);
   }
 };
-
+// can destruct ownProps second arg, since all we need is params.
+// route's filter is now available.
 const mapStateToProps = (state, { params }) => ({
   todos: getVisibleTodos(state.todos, params.filter || 'all'),
 });
 
+// wrap connect in withRouter so that connect gets the props directly
+// from the route in Root.  Binds a child component to a route
+// that is an ancestor of it, basically.
+// withRouter is useful when you need to read route params somewhere
+// deep in the component tree.
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
