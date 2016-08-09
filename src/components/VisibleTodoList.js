be8fe76675614e8e2ecd63665b44bf2916df9a4f
Gurba,Chris <Chris.Gurba@Cerner.com>
2016-08-09 11:43:53 -0500

    add notes for 14

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -6,17 +6,24 @@ import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
 import { fetchTodos } from '../api';
 
+// spread VisibleTodoList into a class based Component.
+// add api logic so we can call it on lifecycle hooks
+// componentDidMount (first load) and componentDidUpdate
+// (subsequent loads/updates)
 class VisibleTodoList extends Component {
   componentDidMount() {
     this.fetchData();
   }
 
   componentDidUpdate(prevProps) {
+    // check that the filter is different.  we don't need ot update
+    // if filter hasn't changed.  Maybe he just added a todo or something.
+    // this catches route changes though.
     if (this.props.filter !== prevProps.filter) {
       this.fetchData();
     }
   }
-
+  //wrapper function for api call.
   fetchData() {
     const { filter, receiveTodos } = this.props;
     fetchTodos(filter).then(todos =>
@@ -42,6 +49,9 @@ VisibleTodoList.propTypes = {
 };
 
 const mapStateToProps = (state, { params }) => {
+  // pass filter as well in return props object,
+  // this allows it to be available in the VisibleTodoList
+  // component props.
   const filter = params.filter || 'all';
   return {
     todos: getVisibleTodos(state, filter),
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import todos, * as fromTodos from './todos';
 
+// move fetchTodos to component (VisibleTodoList:
+//fetchTodos('all').then(todos =>
+//   console.log(todos)
+// );
+
 const todoApp = combineReducers({
   todos,
 });
