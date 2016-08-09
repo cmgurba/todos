d322a3ff19b9704e547ec25befeb9fc818715025
Gurba,Chris <Chris.Gurba@Cerner.com>
2016-08-09 13:29:33 -0500

    notes on 19

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 const byId = (state = {}, action) => {
   switch (action.type) {
     case 'RECEIVE_TODOS': // eslint-disable-line no-case-declarations
+      // shallow copy of state so the function remains pure
       const nextState = { ...state };
       action.response.forEach(todo => {
         nextState[todo.id] = todo;
@@ -12,7 +13,9 @@ const byId = (state = {}, action) => {
       return state;
   }
 };
-
+// next 3 reducer functions just return current state
+// if the action.filter doesn't match up with
+// the reducer's purpose
 const allIds = (state = [], action) => {
   if (action.filter !== 'all') {
     return state;
@@ -48,7 +51,13 @@ const completedIds = (state = [], action) => {
       return state;
   }
 };
-
+// another combine level : each filter of ids
+// in its own property.
+// note on first call by an action (which is triggered by visibleTodoList
+// dispatch) it'll hit our fake api and be "slower".  Subsequent
+// calls to the same sub reducer below will be faster
+// because it won't hit the "RECEIVE_TODOS" flag (api calls only)
+// result is that its just a filter state changing(router).
 const idsByFilter = combineReducers({
   all: allIds,
   active: activeIds,
@@ -62,6 +71,8 @@ const todos = combineReducers({
 
 export default todos;
 
+// selector now expects byId and idsByFilter 
+// to be on the state using combinereducers.
 export const getVisibleTodos = (state, filter) => {
   const ids = state.idsByFilter[filter];
   return ids.map(id => state.byId[id]);
